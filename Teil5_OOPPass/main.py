5eba839d14421ed7ef5cc69d0e7b0d13cc080c5e
Holger Trampe <htrampe@gmail.com>
2023-12-16 21:51:43 +0100

    Updates

---
+++ Teil5_OOPPass/main.py
@@ -0,0 +1,6 @@
+from password import Password
+
+p1 = Password(service="Google", key="Mein Passwort!")
+p2 = Password(service="Mila", key="12345")
+
+print(p2)
---
+++ Teil5_OOPPass/password.py
@@ -0,0 +1,37 @@
+class Password:
+
+    _key = ""
+    _service = ""
+    _hint = ""
+
+    def __init__(self, key, service, hint=""):
+        self._key = key
+        self._service = service
+        self._hint = hint
+
+    @property
+    def service(self):
+        return self._service
+
+    @service.setter
+    def service(self, value):
+        self._service = value
+
+    @property
+    def hint(self):
+        return self._hint
+
+    @hint.setter
+    def hint(self, value):
+        self._hint = value
+
+    @property
+    def key(self):
+        return self._key
+
+    @key.setter
+    def key(self, value):
+        self._key = value
+
+    def __str__(self):
+        return f"Service: {self._service}"
